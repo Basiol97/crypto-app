dfcb6d90e0db64f59c9bebe8e9c5ffc409c5762e
Basil Rahim <basilrahim@Basils-MacBook-Pro.local>
2023-02-25 23:49:24 +0000

    remove-window-resize

--- src/pages/CoinList/CoinList.tsx
+++ src/pages/CoinList/CoinList.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { CCarousel, CCarouselItem } from "@coreui/react";
 import "@coreui/coreui/dist/css/coreui.min.css";
 import CryptoTable from "components/CryptoTable/index";
@@ -18,49 +17,28 @@ import {
 } from "pages/CoinList/CoinList.styled";
 
 const CoinList: React.FC = () => {
-  const [windowSize, setWindowSize] = useState([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-    window.addEventListener("resize", handleWindowResize);
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  });
-
   return (
     <CoinListOuterContainer>
       <CoinListInnerContainer>
         <ChartsOuterContainer>
           <OverView> Overview</OverView>
           <ChartsInnerContainer>
-            {windowSize[0] <= 850 && (
-              <CarouselContainer>
-                <CCarousel controls>
-                  <CCarouselItem>
-                    <LineChart />
-                  </CCarouselItem>
-                  <CCarouselItem>
-                    <BarChart />
-                  </CCarouselItem>
-                </CCarousel>
-              </CarouselContainer>
-            )}
-            {windowSize[0] > 850 && (
-              <>
-                <LineChartContainer>
+            <CarouselContainer>
+              <CCarousel controls>
+                <CCarouselItem>
                   <LineChart />
-                </LineChartContainer>
-                <BarChartContainer>
+                </CCarouselItem>
+                <CCarouselItem>
                   <BarChart />
-                </BarChartContainer>
-              </>
-            )}
+                </CCarouselItem>
+              </CCarousel>
+            </CarouselContainer>
+            <LineChartContainer>
+              <LineChart />
+            </LineChartContainer>
+            <BarChartContainer>
+              <BarChart />
+            </BarChartContainer>
           </ChartsInnerContainer>
         </ChartsOuterContainer>
         <ChartsPeriodConverter />
